444db19ab281d79661c6fdaf2beadb845fae8e50
Javier Coyoc <2024160552@ub.edu.bz>
2025-11-17 20:43:48 -0600

    Implemented trie class

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++)
+            children[i] = nullptr;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+};
+
+int main() {
+
+    return 0;
+}
